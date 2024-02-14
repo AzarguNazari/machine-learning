5ef363453e68bc4ca06c0e372fc8ed0199ca31da
hazar.nazari <hazar.nazari@check24.de>
2024-02-14 21:21:55 +0100

    updated file

---
+++ dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pip list
+pip install numpy
+pip install matplotlib
+pip install scikit-image
+pip install scikit-learn
+pip install pandas
+
+# install jupter-notebook
+#pip install jupyterlab
+#pip install notebook
